238a8ac468efbf743a44df15e2254cb9b5f40315
ShaniceS13 <shanicesolomon2@gmail.com>
2025-05-18 11:40:51 -0400

    Added basic JavaScript

---
+++ src/index.js
@@ -0,0 +1,13 @@
+function generateRecipe(event) {
+  event.preventDefault();
+
+  new Typewriter("#recipe", {
+    strings: "Recipe here",
+    autoStart: true,
+    delay: 2,
+    cursor: "",
+  });
+}
+
+let recipeFormElement = document.querySelector("#recipe-generator-form");
+recipeFormElement.addEventListener("submit", generateRecipe);
